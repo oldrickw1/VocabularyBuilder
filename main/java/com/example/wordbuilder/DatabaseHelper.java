ab7c3a235c1919d695d85f5114a28a4b654c30e9
Oldrick Weenink <o.weenink@hotmail.com>
2023-01-30 10:05:11 +0100

    Refactored DatabaseHelper. Extracted methods, cleaned up code

--- main/java/com/example/wordbuilder/DatabaseHelper.java
+++ main/java/com/example/wordbuilder/DatabaseHelper.java
@@ -17,6 +17,10 @@ import java.util.List;
 public class DatabaseHelper extends SQLiteOpenHelper {
     private final String TAG = "MyApp.DatabaseHelper";
 
+    private SQLiteDatabase db;
+    String foreignTableName;
+    String targetTableName;
+
     public DatabaseHelper(@Nullable Context context) {
         super(context,  "translations.db" , null, 1);
     }
@@ -32,32 +36,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
     public void addOne(Translation translation) {
-        SQLiteDatabase db = this.getWritableDatabase();
+        db = this.getWritableDatabase();
+        foreignTableName = getOrCreateTable(translation.getForeignLanguage(), db);
+        targetTableName = getOrCreateTable(translation.getTargetLanguage(), db);
 
-        String foreignTableName = getOrCreateTable(translation.getForeignLanguage(), db);
-        String targetTableName = getOrCreateTable(translation.getTargetLanguage(), db);
+        long foreignId = insertIntoForeignTable(translation);
+        List<Long> targetIdList = insertIntoTargetTable(translation);
 
-        // inserting into foreign language table
-        ContentValues cvForeign = new ContentValues();
-        cvForeign.put("WORD", translation.getForeignWord());
-        long foreignId = db.insert(foreignTableName, null, cvForeign);
+        insertIntoLinkedTable(foreignId, targetIdList);
 
-        // inserting into target language table
-        List<Long> targetIdList = new ArrayList<>();
-        for (String uniqueTranslation : translation.getTargetLanguageTranslations()) {
-            ContentValues cvTarget = new ContentValues();
-            cvTarget.put("WORD", uniqueTranslation);
-            targetIdList.add(db.insert(targetTableName, null, cvTarget));
-        }
-
-        // now we can insert to the linked table
-        for (long targetId : targetIdList) {
-            ContentValues cv = new ContentValues();
-            cv.put("FOREIGN_ID_COLUMN", foreignId);
-            cv.put("TARGET_ID_COLUMN", targetId);
-            db.insert("TRANSLATIONS", null, cv);
-        }
         db.close();
+    }
+
+
+    public List<Translation> getTranslations() {
+        SQLiteDatabase db = getReadableDatabase();
+
+        String foreignTable = MyApplication.activeForeignLanguage.toString();
+        String targetTable = MyApplication.activeTargetLanguage.toString();
+
+        String queryString = "SELECT " + foreignTable + ".WORD, " + targetTable + ".WORD FROM TRANSLATIONS INNER JOIN" +
+                " " + foreignTable + " ON TRANSLATIONS.FOREIGN_ID_COLUMN = " + foreignTable + ".ID INNER JOIN" +
+                " " + targetTable + " ON TRANSLATIONS.TARGET_ID_COLUMN = " + targetTable + ".ID";
+
+        Cursor cursor = db.rawQuery(queryString, null);
+        List<Translation> translationList = extractFromCursor(cursor);
+        cursor.close();
+        db.close();
+        return translationList;
     }
 
     private String getOrCreateTable(Language language, SQLiteDatabase db) {
@@ -68,32 +74,42 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return tableName;
     }
 
-    public List<Translation> getTranslations() {
-        SQLiteDatabase db = getReadableDatabase();
-
-        List<Translation> translationList = new ArrayList<>();
-
-        String foreignTable = MyApplication.activeForeignLanguage.toString();
-        String targetTable = MyApplication.activeTargetLanguage.toString();
-
-        String queryString = "SELECT " + foreignTable + ".WORD, " +  targetTable + ".WORD FROM TRANSLATIONS INNER JOIN"+
-                " " + foreignTable + " ON TRANSLATIONS.FOREIGN_ID_COLUMN = " + foreignTable + ".ID INNER JOIN"+
-                " " + targetTable + " ON TRANSLATIONS.TARGET_ID_COLUMN = " + targetTable + ".ID";
-
-        Cursor cursor = db.rawQuery(queryString, null);
-        if (cursor.moveToFirst()) {
-            do {
-                String foreignLanguageWord = cursor.getString(0);
-                String targetLanguageTranslation = cursor.getString(1);
-                Log.i(TAG, "foreignLanguageWord: " + foreignLanguageWord + ", targetLanguageTranslation: " + targetLanguageTranslation);
-                translationList.add(new Translation(foreignLanguageWord, targetLanguageTranslation));
-            } while (cursor.moveToNext());
-        } else {
-            // list is empty
+    @NonNull
+    private List<Long> insertIntoTargetTable(Translation translation) {
+        List<Long> targetIdList = new ArrayList<>();
+        for (String uniqueTranslation : translation.getTargetLanguageTranslations()) {
+            ContentValues cvTarget = new ContentValues();
+            cvTarget.put("WORD", uniqueTranslation);
+            targetIdList.add(db.insert(targetTableName, null, cvTarget));
         }
-        cursor.close();
-        db.close();
-        Log.i(TAG, "translationList: " + translationList.get(0).toString());
-        return translationList;
+        return targetIdList;
+    }
+
+    private long insertIntoForeignTable(Translation translation) {
+        ContentValues cvForeign = new ContentValues();
+        cvForeign.put("WORD", translation.getForeignWord());
+        long foreignId = db.insert(foreignTableName, null, cvForeign);
+        return foreignId;
+    }
+
+    private void insertIntoLinkedTable(long foreignId, List<Long> targetIdList) {
+        for (long targetId : targetIdList) {
+            ContentValues cv = new ContentValues();
+            cv.put("FOREIGN_ID_COLUMN", foreignId);
+            cv.put("TARGET_ID_COLUMN", targetId);
+            db.insert("TRANSLATIONS", null, cv);
+        }
+    }
+
+
+    private List<Translation> extractFromCursor(Cursor cursor) {
+        if (cursor.moveToFirst()) {
+            List<Translation> list = new ArrayList<>();
+            do {
+                list.add(new Translation(cursor.getString(0), cursor.getString(1)));
+            } while (cursor.moveToNext());
+            return list;
+        }
+        return null;
     }
 }
