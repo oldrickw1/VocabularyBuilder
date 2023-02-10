cb39389eef6f5baa269a9ef89d26a3c5eb609399
Oldrick Weenink <o.weenink@hotmail.com>
2023-02-10 12:16:43 +0100

    Cleaned up some code, deleted some redundant lines. I'm a bit stuck at this point. Will have to expand knowledge on asynchronous programming and Kotlin in order to proceed

--- main/java/com/example/wordbuilder/Translation.java
+++ main/java/com/example/wordbuilder/Translation.java
@@ -29,48 +29,26 @@ public class Translation {
         return foreignWord;
     }
 
-    public void setForeignWord(String foreignWord) {
-        this.foreignWord = foreignWord;
-    }
 
     public List<String> getTargetLanguageTranslations() {
         return targetLanguageTranslations;
     }
 
-    public void setTargetLanguageTranslations(List<String> targetLanguageTranslations) {
-        this.targetLanguageTranslations = targetLanguageTranslations;
-    }
 
     public Language getTargetLanguage() {
         return targetLanguage;
     }
 
-    public void setTargetLanguage(Language targetLanguage) {
-        this.targetLanguage = targetLanguage;
-    }
 
     public Language getForeignLanguage() {
         return foreignLanguage;
     }
 
-    public void setForeignLanguage(Language foreignLanguage) {
-        this.foreignLanguage = foreignLanguage;
-    }
 
 
     public String getTargetWord() {
         return targetWord;
     }
 
-    public void setTargetWord(String targetWord) {
-        this.targetWord = targetWord;
-    }
 
-    public LevelOfMastery getLevelOfMastery() {
-        return levelOfMastery;
-    }
-
-    public void setLevelOfMastery(LevelOfMastery levelOfMastery) {
-        this.levelOfMastery = levelOfMastery;
-    }
 }
